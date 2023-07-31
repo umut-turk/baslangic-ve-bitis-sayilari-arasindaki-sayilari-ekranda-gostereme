ca066b065d1d996ae00b591fc8eab4f3c123630c
umut-turk <113532432+umut-turk@users.noreply.github.com>
2023-07-31 21:34:35 +0300

    baslangic ve bitis sayilari arasindaki sayilari ekranda gostereme

---
+++ baslangic ve bitis/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baslangic_ve_bitis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.WriteLine("başlangıc  degerı   gırın");
+
+
+
+
+            try
+            {
+                  int baslanic = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("bitiş  degerı   gırın");
+                  int bitis = int.Parse(Console.ReadLine());
+
+                for (int i = baslanic; i <= bitis; i++)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            catch (FormatException ex)
+            {
+
+                Console.WriteLine("yanliş  deger"  + "   "  + "hata  kodu" +"    "  +ex.Message);
+
+            }
+            
+
+          
+
+          
+
+            Console.ReadLine();
+
+        }
+    }
+}
